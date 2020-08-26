a093c890a1539b9be84cfd5b048fdf7f64a679f7
igorovic <758832+igorovic@users.noreply.github.com>
2020-08-26 20:46:40 +0200

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,48 @@ import commonjs from '@rollup/plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+
+/* for smelte */
+//import includePaths from "rollup-plugin-includepaths";
+
 import {rollup as config} from 'mangoost';
 import pkg from './package.json';
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const smelte = require("smelte/rollup-plugin-smelte");
+
+const smelteConfig = {
+    purge: !dev,
+    output: "public/tailwind.css", // it defaults to static/global.css which is probably what you expect in Sapper
+    postcss: [], // Your PostCSS plugins
+    whitelist: [], // Array of classnames whitelisted from purging
+    whitelistPatterns: [], // Same as above, but list of regexes
+    tailwind: {
+      theme: {
+        extend: {
+          spacing: {
+            72: "18rem",
+            84: "21rem",
+            96: "24rem"
+          }
+        }
+      }, // Extend Tailwind theme
+      colors: {
+        primary: "#b027b0",
+        secondary: "#009688",
+        error: "#f44336",
+        success: "#4caf50",
+        alert: "#ff9800",
+        blue: "#2196f3",
+        dark: "#212121"
+      }, // Object of colors to generate a palette from, and then all the utility classes
+      darkMode: true,
+    }, // Any other props will be applied on top of default Smelte tailwind.config.js
+  }
+
 
 const onwarn = (warning, onwarn) =>
 	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
@@ -30,12 +66,13 @@ export default {
 				hydratable: true,
 				emitCss: true
 			}),
+			!dev && smelte(smelteConfig),
 			resolve({
 				browser: true,
 				dedupe: ['svelte']
 			}),
 			commonjs(),
-
+			//includePaths({ paths: ["./src", "./", "./node_modules/smelte/src/"] }), // smelte
 			legacy && babel({
 				extensions: ['.js', '.mjs', '.html', '.svelte'],
 				babelHelpers: 'runtime',
@@ -75,9 +112,11 @@ export default {
 				hydratable: true,
 				dev
 			}),
+			smelte(smelteConfig),
 			resolve({
 				dedupe: ['svelte']
 			}),
+			//includePaths({ paths: ["./src", "./", "./node_modules/smelte/src/"] }),
 			commonjs()
 		],
 		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
@@ -86,7 +125,7 @@ export default {
 		onwarn,
 	},
 
-	serviceworker: {
+	/* serviceworker: {
 		input: config.serviceworker.input(),
 		output: config.serviceworker.output(),
 		plugins: [
@@ -101,5 +140,5 @@ export default {
 
 		preserveEntrySignatures: false,
 		onwarn,
-	}
+	} */
 };
