8bdfc19ed4dbd8e35fa6d1dfcdbdf06e8d39b5a1
Petrovic Igor <petroi@esr.ch>
2020-08-27 16:11:02 +0200

    remove sapper

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,22 @@
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
-import svelte from 'rollup-plugin-svelte';
+//import svelte from 'rollup-plugin-svelte';
+import svelte from 'mangoost/dist/rollup-plugins/svelte';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
-/* for smelte */
-//import includePaths from "rollup-plugin-includepaths";
 
-import {rollup as config} from 'mangoost';
 import pkg from './package.json';
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-const smelte = require("smelte/rollup-plugin-smelte");
+//const smelte = require("smelte/rollup-plugin-smelte");
 
 const smelteConfig = {
-    purge: !dev,
+    purge: true,
     output: "public/tailwind.css", // it defaults to static/global.css which is probably what you expect in Sapper
     postcss: [], // Your PostCSS plugins
     whitelist: [], // Array of classnames whitelisted from purging
@@ -53,77 +51,60 @@ const onwarn = (warning, onwarn) =>
 	onwarn(warning);
 
 export default {
-	client: {
-		input: config.client.input(),
-		output: config.client.output(),
-		plugins: [
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			svelte({
-				dev,
-				hydratable: true,
-				emitCss: true
-			}),
-			!dev && smelte(smelteConfig),
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-			commonjs(),
-			//includePaths({ paths: ["./src", "./", "./node_modules/smelte/src/"] }), // smelte
-			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				babelHelpers: 'runtime',
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
-			}),
-
-			!dev && terser({
-				module: true
-			})
-		],
-
-		preserveEntrySignatures: false,
-		onwarn,
+	
+	input: 'src/pages/main.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: 'public/bundle.js',
 	},
+	plugins: [
+		replace({
+			'process.browser': true,
+			'process.env.NODE_ENV': JSON.stringify(mode)
+		}),
+		svelte({
+			dev,
+			hydratable: true,
+			generate: 'ssr',
+			emitCss: true,
+			css: true
+		}),
+		//smelte(smelteConfig),
+		resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+		commonjs(),
+		
+		legacy && babel({
+			extensions: ['.js', '.mjs', '.html', '.svelte'],
+			babelHelpers: 'runtime',
+			exclude: ['node_modules/@babel/**'],
+			presets: [
+				['@babel/preset-env', {
+					targets: '> 0.25%, not dead'
+				}]
+			],
+			plugins: [
+				'@babel/plugin-syntax-dynamic-import',
+				['@babel/plugin-transform-runtime', {
+					useESModules: true
+				}]
+			]
+		}),
 
-	server: {
-		input: config.server.input(),
-		output: config.server.output(),
-		plugins: [
-			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			svelte({
-				generate: 'ssr',
-				hydratable: true,
-				dev
-			}),
-			smelte(smelteConfig),
-			resolve({
-				dedupe: ['svelte']
-			}),
-			//includePaths({ paths: ["./src", "./", "./node_modules/smelte/src/"] }),
-			commonjs()
-		],
-		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+		!dev && terser({
+			module: true
+		})
+	],
 
-		preserveEntrySignatures: 'strict',
-		onwarn,
-	},
+	preserveEntrySignatures: false,
+	onwarn,
+
+
+	
 
 	/* serviceworker: {
 		input: config.serviceworker.input(),
--- src/routes/about.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-	<title>About</title>
-</svelte:head>
-
-<h1 class="text-3xl">About this site</h1>
-
-<p>This is the 'about' page. There's not much here.</p>
