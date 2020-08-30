408bc5df8fc2f162bfa3a2ffc34230f3b2874191
igorovic <758832+igorovic@users.noreply.github.com>
2020-08-30 18:51:01 +0200

    wip

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,43 +1,51 @@
+<script context="ssr">
+    export const initialData = {
+        url: "https://api.mocki.io/v1/e527fc1f"
+    };
+</script>
 <script context="module">
-	export const initialData = {
-		url: "https://api.mocki.io/v1/e527fc1f"
-	};
+    export const initialData = {
+        "init": "init with this data"
+    };
 </script>
 <script>
-	import Nav from '../components/Nav.svelte';
-	export let name;
-	export let message;
+    import Nav from '../components/Nav.svelte';
+    import {initialData as iNav} from '../components/Nav.svelte';
+    export let name;
+    export let message;
+    console.log(iNav);
 </script>
 
 <svelte:head>
-	<title>App title</title>
+    <title>App title</title>
 </svelte:head>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	
-	<Nav />
-	<h2>message</h2>
-	<p>{message}</p>
+    <h1>Hello {name}!</h1>
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    
+    <Nav />
+    <h2>message</h2>
+    <p>{message}</p>
+    <p>{initialData}</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
